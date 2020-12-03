5e433df60b1545b30a4b4280519b80f6a721217a
Vitalii Trachenko <vitaltrachenko@gmail.com>
2020-12-03 14:46:33 +0200

    feat: added ability receive zeromq messages from zeromq-server

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,20 @@
-import {Dealer} from "zeromq"
-
-import {Queue} from "./queue"
+import {Dealer} from "zeromq";
 
 async function main() {
-  const sender = new Dealer()
-  await sender.bind("tcp://127.0.0.1:5555")
-
-  const queue = new Queue(sender)
-  queue.send("hello")
-  queue.send("world!")
-  queue.send(null)
-
-  const receiver = new Dealer()
-  receiver.connect("tcp://127.0.0.1:5555")
+  const receiver = new Dealer();
+  receiver.connect("tcp://127.0.0.1:5555");
 
   for await (const [msg] of receiver) {
     if (msg.length === 0) {
-      receiver.close()
-      console.log("received: <empty message>")
+      receiver.close();
+      console.log("received: <empty message>");
     } else {
-      console.log(`received: ${msg}`)
+      console.log(`received: ${msg}`);
     }
   }
 }
 
 main().catch(err => {
-  console.error(err)
-  process.exit(1)
+  console.error(err);
+  process.exit(1);
 })
