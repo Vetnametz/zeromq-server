581810a7527dd99c14f5792ea6b854e731a44119
Vitalii Trachenko <vitaltrachenko@gmail.com>
2020-12-12 22:32:49 +0200

    feat: generate sensor parameters with interval

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import {Dealer} from "zeromq"
+import { Dealer } from "zeromq"
 import { ReactIR } from "./model";
 
-import {Queue} from "./queue"
+import { Queue } from "./queue"
 
 async function main() {
-  const reactIRData = new ReactIR().generateFakeReactIR();
+
   const sender = new Dealer();
   await sender.bind("tcp://127.0.0.1:5555");
 
   const queue = new Queue(sender);
-  queue.send(JSON.stringify(reactIRData));
+  setInterval(function () {
+    console.log("generating fake reactIR parameters");
+    const reactIRData = new ReactIR().generateFakeReactIR();
+    queue.send(JSON.stringify(reactIRData));
+    queue.send(null);
+  }, 60000);
+
 }
 
 main().catch(err => {
